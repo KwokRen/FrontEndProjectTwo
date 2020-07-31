064ffc1084009e7d49d94137b900ed7f6c41c40f
Kwok <kwokrenx@gmail.com>
2020-07-31 14:35:25 -0400

    removed some comments

--- app.js
+++ app.js
@@ -8,13 +8,3 @@ $hello.hover (function () {
 })
 
 
-// const $buttonglow = function(name, button, link) {
-//     let $name = $(`.${name}`);
-//     $name.hover(function() {
-//     $(`.${button}`).css('background-color','#dbf5ff');
-//     $(`.${link}`).css('color','black');
-//     }, function () {
-//     $(`.${button}`).css('background-color',"#19415C");
-//     $(`.${link}`).css('color','white');
-//     })
-//   };
